68ddff8bac9df4bdb68a698ecd9f63c23bbeb4a1
Noka Kipiani <noka.kipiani@wandio.com>
2019-09-30 12:31:18 +0400

    add books data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import axios from "axios";
 import Navbar from "../src/components/Navbar";
 import Home from "../src/components/Home";
 import Details from "../src/components/Details";
 import Favorites from "../src/components/Favorites";
 
 class App extends Component {
+  state = {
+    books: []
+  };
+  componentDidMount() {
+    axios
+      .get(
+        "https://www.googleapis.com/books/v1/volumes?q=harry&fbclid=IwAR0GNT0p1cF3NPkf81hy-ZSfPcGFGRK6WdAXWoBORurTiwrACCpPUHUOSgE"
+      )
+      .then(response => {
+        this.setState({ books: response.data.items });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   render() {
+    const { books } = this.state;
     return (
       <BrowserRouter>
         <div>
           <Navbar />
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Home} data={books} />
           <Route path="/Details" component={Details} />
           <Route path="/Favorites" component={Favorites} />
         </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,12 @@
 import React, { Component } from "react";
 import { Table } from "reactstrap";
-import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
 class Home extends Component {
-  state = {
-    books: []
-  };
-  componentDidMount() {
-    axios
-      .get(
-        "https://www.googleapis.com/books/v1/volumes?q=harry&fbclid=IwAR0GNT0p1cF3NPkf81hy-ZSfPcGFGRK6WdAXWoBORurTiwrACCpPUHUOSgE"
-      )
-      .then(response => {
-        this.setState({ books: response.data.items });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
   render() {
-    console.log(this.state.books);
+    const { books } = this.props;
     return (
       <div>
         <Table>
@@ -36,7 +19,7 @@ class Home extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.books.map(book => {
+            {this.props.books.map(book => {
               return (
                 <tr key={book.id}>
                   <td>{book.kind}</td>
